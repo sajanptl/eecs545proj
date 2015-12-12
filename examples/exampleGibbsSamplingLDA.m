7553a077bc08007a386af5d6e3a0bc3158fc4ab6
Xiang Li <seanlee1990@gmail.com>
2015-12-12 00:01:43 -0500

    Added GibbsSamplingLDA example using AP dataset

--- examples/exampleGibbsSamplingLDA.m
+++ examples/exampleGibbsSamplingLDA.m
@@ -6,5 +6,21 @@
 original_path = pwd
 
 % read data
-cd 'data/ap/LDA_input'
+cd('../data/ap/LDA_input/')
+load 'WS.mat'
+load 'DS.mat'
+load 'WO.mat'
+
+cd(original_path)
+
+T = 10;
+N_round = 1;
+ALPHA = 1;
+BETA = 1;
+SEED = 3;
+
+cd('../lib/')
+[ WP,DP,Z ] = GibbsSamplerLDA( WS , DS , T , N_round , ALPHA , BETA , SEED)
+
+
 
--- lib/GibbsSamplerLDA.m
+++ lib/GibbsSamplerLDA.m
@@ -3,9 +3,8 @@
 % Author: Xiang Li
 % 12/10/2015
 
-function [ WP,DP,Z ] = GibbsSamplerLDA( WS , DS , T , N , ALPHA , BETA , SEED , OUTPUT )
+function [ WP,DP,Z ] = GibbsSamplerLDA( WS , DS , T , N_round , ALPHA , BETA , SEED)
 % Input:
-%
 % WS - N x 1 vector where WS(k) contains the vocabulary index of the kth
 % word token, and N is the number of word tokens. The word indices are not zero
 % based, i.e., min( WS )=1 and max( WS ) = W = number of distinct words in
@@ -17,47 +16,62 @@ function [ WP,DP,Z ] = GibbsSamplerLDA( WS , DS , T , N , ALPHA , BETA , SEED , 
 %
 % T - number of topics
 % 
-% N - number of iterations
+% N_round - number of iterations
 % 
 % ALPHA - Parameter that defines the symmetric Dirichlet distribution for topic assignment
 % 
 % BETA - Parameter that defines the symmetric Dirichlet distribution for
 % word generation
+% 
+% Output:
+% WP - sparse matrix of size W x T, where WP(i,j) contains the number of 
+% times word i has been assigned to topic j. (related with beta_k)   
+% 
+% DP - a sparse matrix of size D x T matrix, where DP(i,j) contains the 
+% number of times a word in document d has been assigned to topic j. 
+% 
+% Z - topic assignment for token k.  
 
+N = length(WS);   % N - total number of words 
+W = max(WS);   % W - number of distinct words in volcabulary
 
-% Randomly assign each token to one topic
-TS = randi([1, T], 1, N);
+% randomly assign each token to one topic
+Z = randi([1, T], 1, N);
 
-for i = 1:N
-    reassign( TS, WS, DS, T);
+% update the Gibbs sampler
+for i = 1:N_round
+    reassign(Z, WS, DS, T, ALPHA, BETA);
 end
 
+WP = sparse(W, T);
+DP = sparse(D, T);
+
 end
 
 
 
-function reassign( TS, WS, DS, T)
+function reassign( Z, WS, DS, T, ALPHA, BETA)
 
-n_tokens = len(TS)
-for w = 1:n_tokens
-    p = zeros(T, 1);
-    for j = 1:T
-        % number of tokens in document d
-        nd = sum(DS == d(w));
+N = length(Z)
+for w = 1:N   % for every word
+    p = zeros(T, 1);   % probability distribution for reassigning
+    
+    d = DS(w);    % document d is the document containing word w
+    
+    nd = sum(DS == d) - 1;   % number of words in document d (except current word) 
+    
+    nwd = sum( (WS == w) .* (DS == d) ) - 1;   % times of word w occuring in document d (except current word)
+    
+    for j = 1:T  % for every topic
+        nj = sum(Z == j) - 1;   % total number of words in topic j (except current word)
 
-        % number of token w in document d
-        nwd = sum(WS == w && DS == d(w));
+        nwj = sum( (WS == w) .* (Z == j) ) - 1;   % times of word w assigned to topic j (except current word)
 
-        % number of all tokens in topic j
-        nj = sum(TS == j);
-
-        % number of token w in topic j
-        nwj = sum(WS == w && TS == j);
-
-        p[j] = nwd / nd * nwj / nj;
+        p(j) = (nwd + BETA) / (nd + N*BETA) * (nwj + ALPHA) / (nj + T*ALPHA);
     end
+    
     p = p / sum(p);
-    TS(w) = randsample(1:T, 1, true, p);
+    Z(w) = randsample(1:T, 1, true, p);
 end
 
 end
