a0399203ff3d25c5ea73049f54c9a0ea01674021
LozenPoi <zluo@umich.edu>
2015-12-12 17:29:33 -0500

    Issues Fixed for Variational Inference

--- lib/ELB.m
+++ lib/ELB.m
@@ -20,14 +20,14 @@ function L=ELB(gamma,phi,alpha,beta)
 % Author: Z. Luo
 % Date: December 2015
 
+%{
 L1=gammaln(sum(alpha))-sum(gammaln(alpha))+...
      psi(sum(gamma,1)-psi(sum(sum(gamma))))*(alpha-1);
 
-L2=(psi(sum(gamma,2))-psi(sum(sum(gamma))))*sum(sum(phi,2),3);    
+L2=(psi(sum(gamma,2))-psi(sum(sum(gamma))))'*sum(sum(phi,2),3);    
 
-%phi_kn=sum(phi,3)';
-phi_kn=sum(phi,1)';    
-L3=sum(beta.*phi_kn,1);
+phi_kn=sum(permute(phi,[2 3 1]),3)';    
+L3=sum(sum(beta.*phi_kn));
 
 L4=-gammaln(sum(sum(gamma)))+sum(gammaln(sum(gamma)))+...
       (sum(gamma,1)-1)*psi(sum(gamma,1)-psi(sum(sum(gamma))))';
@@ -35,5 +35,28 @@ L4=-gammaln(sum(sum(gamma)))+sum(gammaln(sum(gamma)))+...
 L5=-sum(sum(sum(phi.*log(phi))));
 
 L=L1+L2+L3+L4+L5;
+%}
+
+
+
+J_t1=gammaln(sum(alpha))-sum(gammaln(alpha))+...
+     dot((alpha-1),psi(sum(gamma,1)-psi(sum(sum(gamma)))));
+
+J_t2=dot(sum(sum(phi,2),3), psi(sum(gamma,2))-psi(sum(sum(gamma))));    
+
+beta_idx=find(beta(:)); phi_kn=sum(phi,3)';    
+J_t3=dot(beta(beta_idx),phi_kn(beta_idx));
+
+J_t4=-gammaln(sum(sum(gamma)))+sum(gammaln(sum(gamma)))+...
+      dot((sum(gamma,1)-1),psi(sum(gamma,1)-psi(sum(sum(gamma)))));
+
+phi_idx=find(phi);
+J_t5=-dot(phi(phi_idx),log(phi(phi_idx)));
+
+L=J_t1+J_t2+J_t3+J_t4+J_t5;
+
+
+
+
 
 end
