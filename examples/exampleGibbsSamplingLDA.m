2f5a36a69a6fa87bcbd37c38e0f30bd7346c9364
Xiang Li <seanlee1990@gmail.com>
2015-12-14 16:19:36 -0500

    Fixed bugs in GibbsSampling

--- examples/exampleGibbsSamplingLDA.m
+++ examples/exampleGibbsSamplingLDA.m
@@ -3,7 +3,7 @@
 % Xiang Li
 % 12/11/2015
 
-original_path = pwd
+original_path = pwd;
 
 % read data
 cd('../data/ap/LDA_input/')
@@ -11,22 +11,30 @@ load 'WS.mat'
 load 'DS.mat'
 load 'ZWD.mat'
 load 'WO.mat'
+load 'WS_test.mat'
+load 'DS_test.mat'
+load 'ZWD_test.mat'
 
 % create training set and test set (for calculating perplexity)
 
 
 cd(original_path)
 
-T = 10;
-N_round = 1;
+T = 5;
+N_round = 20;
 ALPHA = 1;
-BETA = 1;
+BETA = 0.01;
 SEED = 3;
 
 cd('../lib/')
 tic
-[ WP,DP,Z ] = GibbsSamplerLDA( WS , DS , ZWD, T , N_round , ALPHA , BETA , SEED)
+[ WP,DP,Z,Per ] = GibbsSamplerLDA( WS , DS , ZWD, WO, WS_test, DS_test, ...
+    ZWD_test, T , N_round , ALPHA , BETA , SEED);
 toc
+cd(original_path)
 
-WO = cellstr(WO)
+cd('../lib/')
+WO = cellstr(WO);
+WriteTopics(WP, WO, 'topics.txt');
+cd(original_path)
 
--- lib/GibbsSamplerLDA.m
+++ lib/GibbsSamplerLDA.m
@@ -3,7 +3,8 @@
 % Author: Xiang Li
 % 12/10/2015
 
-function [ WP,DP,Z ] = GibbsSamplerLDA( WS , DS , ZWD, T , N_round , ALPHA , BETA , SEED)
+function [ WP,DP,Z,Per ] = GibbsSamplerLDA( WS , DS , ZWD, WO, WS_test, ...
+    DS_test, ZWD_test, T , N_round , ALPHA , BETA , SEED)
 % Input:
 % WS - N x 1 vector where WS(k) contains the vocabulary index of the kth
 % word token, and N is the number of word tokens. The word indices are not zero
@@ -18,7 +19,8 @@ function [ WP,DP,Z ] = GibbsSamplerLDA( WS , DS , ZWD, T , N_round , ALPHA , BET
 %
 % N_round - number of iterations
 %
-% ALPHA - Parameter that defines the symmetric Dirichlet distribution for topic assignment
+% ALPHA - Parameter that defines the symmetric Dirichlet distribution for 
+%         topic assignment
 %
 % BETA - Parameter that defines the symmetric Dirichlet distribution for
 % word generation
@@ -32,18 +34,10 @@ function [ WP,DP,Z ] = GibbsSamplerLDA( WS , DS , ZWD, T , N_round , ALPHA , BET
 %
 % Z - topic assignment for token k.
 
-N = length(WS);     % N - total number of words
-W = max(WS);        % W - number of distinct words in volcabulary
-D = length(ZWD);    % D - total number of documents
+[N, W, D, ND, NWD] = prepare_data(WS, ZWD, WO);
+[N_test, W_test, D_test, ND_test, NWD_test] = prepare_data(...
+    WS_test, ZWD_test, WO);
 
-ND = sum(ZWD, 2);   % number of words in each document
-NWD = zeros(W, D);  % times of word w occuring in document d (except current word)
-for w = 1:W
-    for d = 1:D
-%         NWD(w, d) = sum( (WS == w) .* (DS == d) ) - 1;
-        NWD(w, d) = sum(ZWD(d,:) == w);
-    end
-end
 
 % randomly assign each token to one topic
 Z = randi([1, T], 1, N)';
@@ -54,56 +48,99 @@ for i = 1:N         % for every word i
     WP(w, Z(i)) = WP(w, Z(i)) + 1;
 end
 
-% update the Gibbs sampler
+
+Per = zeros(N_round, 1);
 for i = 1:N_round
-    Per = reassign(Z, WS, DS, ND, NWD, WP, T, ALPHA, BETA);
+    disp(sprintf('N_round = %d', i))
+    % update the Gibbs sampler
+    [Z, WP] = reassign(Z, WS, DS, ND, NWD, WP, T, ALPHA, BETA);   
+    % Calculate perplexity
+    Per(i) = perplexity(WS_test, DS_test, ND_test, NWD_test, WP, T, ALPHA , BETA);   
+    
+    disp(sprintf('Perplexity = %f', Per(i)))
 end
 
 DP = zeros(D, T);
 
+
 end
 
+%%
+function [N, W, D, ND, NWD] = prepare_data(WS, ZWD, WO)
 
+N = length(WS);     % N - total number of words
+W = length(WO);     % W - number of distinct words in volcabulary
+D = size(ZWD, 1);    % D - total number of documents
 
-function Per = reassign( Z, WS, DS, ND, NWD, WP, T, ALPHA, BETA )
+% number of words (tokens) in each document
+ND = sum(ZWD ~= 0, 2);   
+% times of word w occuring in document d (except current word)
+NWD = zeros(W, D);  
+for w = 1:W
+    for d = 1:D
+        NWD(w, d) = sum(ZWD(d,:) == w);
+    end
+end
+
+end
+
+%%
+function [Z_new, WP_new] = reassign( Z, WS, DS, ND, NWD, WP, T, ALPHA, BETA )
 
 N = length(Z);
+Z_new = zeros(N, 1);
 for i = 1:N   % for every word
-    if (rem(i, 100) == 0)
-        disp(sprintf('Reassigning word %d of %d', i, N))
-    end
+%     if (rem(i, 10000) == 0)
+%         disp(sprintf('Reassigning word %d of %d', i, N))
+%     end
     p = zeros(T, 1);   % probability distribution for reassigning
 
     w = WS(i);    % token represented by word i
     d = DS(i);    % document d is the document containing word i
-
-%     nd = sum(DS == d) - 1;   % number of words in document d (except current word)
     nd = ND(d) - 1;   % number of words in document d (except current word)
-
-%     nwd = sum( (WS == w) .* (DS == d) ) - 1;   % times of word w occuring in document d (except current word)
-    nwd = NWD(w,d) - 1;   % times of word w occuring in document d (except current word)
-
+    % times of token w occuring in document d (except current word)
+    nwd = NWD(w,d) - 1;   
+    
     for j = 1:T  % for every topic
-%         nj = sum(Z == j) - 1;   % total number of words in topic j (except current word)
-        nj = sum(WP(:, j));
-
-%         nwj = sum( (WS == w) .* (Z == j) ) - 1;   % times of word w assigned to topic j (except current word)
-        nwj = WP(w, j);
-
+        % total number of words in topic j (except current word)
+        nj = sum(WP(:, j)) - (Z(i)==j);  
+        % times of token w assigned to topic j (except current word)
+        nwj = WP(w, j) - (Z(i)==j);   
         p(j) = (nwd + BETA) / (nd + N*BETA) * (nwj + ALPHA) / (nj + T*ALPHA);
     end
 
     p = p / sum(p);
-    Z(w) = randsample(1:T, 1, true, p);
+    Z_new(i) = randsample(1:T, 1, true, p);
 end
 
 % update WP
+WP_new = zeros(size(WP));
 for i = 1:N         % for every word i
     w = WS(i);      % token represented by word i
-    WP(w, Z(i)) = WP(w, Z(i)) + 1;
+    WP_new(w, Z(i)) = WP_new(w, Z(i)) + 1;
 end
 
-% calculate perplexity
-Per = 0;
+end
+
+%%
+function per = perplexity( WS_test, DS_test, ND_test, NWD_test, WP, T, ALPHA , BETA)
+
+N = length(WS_test);
+per = 0;
+for i = 1:N            % for every word
+    w = WS_test(i);    % token represented by word i
+    d = DS_test(i);    % document d is the document containing word i
+    nd = ND_test(d);   % number of words in document d (except current word)
+    nwd = NWD_test(w,d);   % times of word w occuring in document d (except current word)
+
+    for j = 1:T  % for every topic
+        nj = sum(WP(:, j));
+        nwj = WP(w, j);
+        p = log10( (nwd + BETA) / (nd + N*BETA) * (nwj + ALPHA) / (nj + T*ALPHA) );
+    end
+
+    per = per + p;
+end
+per = exp(-per/N);
 
 end
