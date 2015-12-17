c56699cdfc3283f03eca8e393fe9c7ae0d601e75
Sajan Patel <sajanptl@umich.edu>
2015-12-16 21:08:47 -0500

    added comments to runUnigram

--- lib/runUnigram.m
+++ lib/runUnigram.m
@@ -1,11 +1,20 @@
+%% Runs the Unigram model on the AP Corpus dataset
+%  Sajan Patel (sajanptl)
+%  December 2015
+
+
+%% Load data
 load ../data/ap/LDA_input/W.mat
 load ../data/ap/LDA_input/W_test.mat
 load ../data/ap/LDA_input/WO.mat
 
+%% smoothing parameter
 lambda = 0.95;
 
+%% run model
 [Ptrain, Ptest, PPlexTrain, PPlexTest, WplexTrain, WplexTest] = Unigram(W, W_test, lambda);
 
+%% print results
 fprintf(['-----------\n']);
 fprintf(['Total Training Perplexity = %f\n'], PPlexTrain);
 fprintf(['Total Testing Perplexity = %f\n'], PPlexTest);
@@ -27,4 +36,5 @@ for i = 1:3
     fprintf(['-----------\n']);
 end
 
+%% save results
 save unigram_test.mat lambda Ptrain Ptest PPlexTrain PPlexTest WplexTrain WplexTest
