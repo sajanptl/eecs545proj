d1f7339b5f2c93f8efc3e25d1ef310a587f81e89
Sajan Patel <sajanptl@umich.edu>
2015-12-16 22:03:02 -0500

    updating plsi sample run and saving output

--- plsa/sampleRun.m
+++ plsa/sampleRun.m
@@ -1,52 +1,52 @@
 
 % % compile the mex files, only needs to be done once
-% mex mex_Pw_d.c -largeArrayDims
-% mex mex_EMstep.c -largeArrayDims
-% mex mex_logL.c -largeArrayDims
-% 
+%{
+mex mex_Pw_d.c -largeArrayDims
+mex mex_EMstep.c -largeArrayDims
+mex mex_logL.c -largeArrayDims
+%}
+
 % %%
 % % load a test dataset
 % load 20News_w100.mat
 
+%%{
 %%
 % set some variables
+%
 Learn.Verbosity = 1;
-Learn.Max_Iterations = 200; 
+Learn.Max_Iterations = 100; 
 Learn.heldout = .1; % for tempered EM only, percentage of held out data
 Learn.Min_Likelihood_Change = 1;
 Learn.Folding_Iterations = 20; % for TEM only: number of fiolding
                                % in iterations
-
+Learn.saveit = 25;
+Learn.sfile = 'plsi_out.mat';
 Learn.TEM = 1; %tempered or not tempered
+%
 %%
 % start pLSA
-Xtrain = double(W(1,:,:));
-Xtest = double(W_test(1,:,:));
+Xtrain = sparse(double(W'));
+Xtest = sparse(double(W_test'));
 
 %%
 [Pw_z,Pz_d,Pd,Li,perp,beta] = pLSA(Xtrain,[],10,Learn);
 
 % fold in test data
-Pz_q = pLSA_EMfold(Xtest,Pw_z,[],25,beta);
+%Pz_q = pLSA_EMfold(Xtest,Pw_z,[],2,beta);
 
 
-Learn.TEM = 0;
-[Pw_z2,Pz_d2,Pd2,Li] = pLSA(Xtrain,[],10,Learn);
+%Learn.TEM = 0;
+%[Pw_z2,Pz_d2,Pd2,Li] = pLSA(Xtrain,[],10,Learn);
 
 Pq = sum(Xtest)./sum(Xtest(:));
 
 
 % fold in test data
-Pz_q2 = pLSA_EMfold(Xtest,Pw_z2,[],25,1);
+%Pz_q2 = pLSA_EMfold(Xtest,Pw_z2,[],2,1);
 
 % and output perplexity
-p1 = pLSA_logL(Xtest,Pw_z,Pz_q,Pq);
-p2 = pLSA_logL(Xtest,Pw_z2,Pz_q2,Pq);
+p1 = pLSA_logL(Xtest,Pw_z,Pz_q,Pq)
+%p2 = pLSA_logL(Xtest,Pw_z2,Pz_q2,Pq)
 
-
-
-
-
-
-
-
+%%}
