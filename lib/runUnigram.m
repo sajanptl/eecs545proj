2b8324f20a4989662ba2e3d4645564a9abfe496f
Sajan Patel <sajanptl@umich.edu>
2015-12-16 21:05:20 -0500

    Fixed perplexity calculation in Unigram model.
    runUnigram now shows the top 10 topics from training the model
    and the top 10 topics from various testing documents. All
    results are saved in unigram_test.mat

--- lib/runUnigram.m
+++ lib/runUnigram.m
@@ -1,21 +1,30 @@
 load ../data/ap/LDA_input/W.mat
 load ../data/ap/LDA_input/W_test.mat
+load ../data/ap/LDA_input/WO.mat
 
 lambda = 0.95;
 
 [Ptrain, Ptest, PPlexTrain, PPlexTest, WplexTrain, WplexTest] = Unigram(W, W_test, lambda);
 
+fprintf(['-----------\n']);
 fprintf(['Total Training Perplexity = %f\n'], PPlexTrain);
 fprintf(['Total Testing Perplexity = %f\n'], PPlexTest);
+fprintf(['-----------\n']);
 
 fprintf(['Top 10 Training Words: \n']);
-[p, idx] = sort(Ptrain(1,:), 'descend');
+[p, idx] = sort(Ptrain, 'descend');
 disp(p(1:10));
 disp(idx(1:10));
+disp(WO(idx(1:10), :));
+fprintf(['-----------\n']);
 
-fprintf(['Top 10 Testing Words: \n']);
-[p, idx] = sort(Ptest(1,:), 'descend');
-disp(p(1:10));
-disp(idx(1:10));
+for i = 1:3
+    fprintf(['Top 10 Testing Words for Document %d: \n'], i*10);
+    [p, idx] = sort(Ptest(i*10,:), 'descend');
+    disp(p(1:10));
+    disp(idx(1:10));
+    disp(WO(idx(1:10), :));
+    fprintf(['-----------\n']);
+end
 
 save unigram_test.mat lambda Ptrain Ptest PPlexTrain PPlexTest WplexTrain WplexTest
